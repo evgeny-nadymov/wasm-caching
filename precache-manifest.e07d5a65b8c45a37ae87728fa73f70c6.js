bfc69f42efca70b196c9a81c7ebcdeb5a5431703
evgeny-nadymov <johnnypmpu@gmail.com>
2019-07-18 19:40:44 +0300

    Updates

---
+++ precache-manifest.e07d5a65b8c45a37ae87728fa73f70c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b1bdc5b85916c8a38a2",
+    "url": "/wasm-caching/static/css/main.9affa070.chunk.css"
+  },
+  {
+    "revision": "5b1bdc5b85916c8a38a2",
+    "url": "/wasm-caching/static/js/main.9cfd2201.chunk.js"
+  },
+  {
+    "revision": "4f3523a587433dabe1a7",
+    "url": "/wasm-caching/static/js/runtime~main.824d9dd1.js"
+  },
+  {
+    "revision": "449e2feec61288286575",
+    "url": "/wasm-caching/static/js/2.4ab32439.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wasm-caching/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ac6f1d431c3b1ea3bb0dfa55f1120d41",
+    "url": "/wasm-caching/index.html"
+  }
+];
